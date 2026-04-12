fd5823df3879a7e015262cd7cd64b82ee8cf9310
Christian Doh Dinga <c.dohdinga@tudelft.nl>
2026-04-12 19:06:29 +0200

    initial commit

---
+++ src/demoses_grid_tariffs/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about demoses_grid_tariffs."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Christian Doh Dinga"
+__email__ = "c.dohdinga@tudelft.nl"
+__version__ = "0.1.0"
